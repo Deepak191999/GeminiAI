d08ced745ffaa0e08567bf701ad5bb46f186ab27
Deepak kumar <deep98.r@gmail.com>
2024-07-15 17:55:18 +0530

    final

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,7 +3,15 @@ import "./Main.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 const Main = () => {
-  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
 
   return (
     <div className="main">
@@ -12,59 +20,79 @@ const Main = () => {
         <img src={assets.user_icon} alt="user_icon" />
       </div>
       <div className="main-container">
-        {!showResult? <>
-          <div className="greet">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
-          <p>How Can I Help You Today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful place to visit in India</p>
-            <img src={assets.compass_icon} alt="compass_icon" />
-          </div>
-          <div className="card">
-            <p>Briefly summarize this concept: Urban Planning</p>
-            <img src={assets.bulb_icon} alt="compass_icon" />
-          </div>
-          <div className="card">
-            <p>BrainStrom team bonding activities for our work retreat</p>
-            <img src={assets.message_icon} alt="compass_icon" />
-          </div>
-          <div className="card">
-            <p>Improve the readibility of following code</p>
-            <img src={assets.code_icon} alt="compass_icon" />
-          </div>
-        </div>
-        </>
-        : <div className="result">
-          <div className="result-title">
-            <img src={assets.user_icon} alt="user_icon" />
-            <p>{recentPrompt}</p>
-          </div>
-          <div className="result-data">
-            <img src={assets.gemini_icon} alt="gemini_icon" />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How Can I Help You Today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful place to visit in India</p>
+                <img src={assets.compass_icon} alt="compass_icon" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: Urban Planning</p>
+                <img src={assets.bulb_icon} alt="compass_icon" />
+              </div>
+              <div className="card">
+                <p>BrainStrom team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="compass_icon" />
+              </div>
+              <div className="card">
+                <p>Improve the readibility of following code</p>
+                <img src={assets.code_icon} alt="compass_icon" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="user_icon" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="gemini_icon" />
 
-            {loading?<div className="loader">
-              <hr />
-              <hr />
-              <hr />
-            </div>:
-            <p dangerouslySetInnerHTML={{__html:resultData}}></p>
-            }
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-        </div>
-       }
-      
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
-            <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  onSent();
+                }
+              }}
+            />
             <div>
               <img src={assets.gallery_icon} alt="gallery_icon" />
               <img src={assets.mic_icon} alt="mic_icon" />
-              <img onClick={()=>onSent()} src={assets.send_icon} alt="send_icon" />
+              {input?<img
+                onClick={() => onSent()}
+                src={assets.send_icon}
+                alt="send_icon"
+              />:
+              null
+              }
+              
             </div>
           </div>
           <p className="bottom-info">
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -15,14 +15,27 @@ const ContextProvider=(props)=>{
             setResultData(prev=> prev+nextWord)
         },75*index );
     }
+    const newChat=()=>{
+        setLoading(false)
+        setShowResult(false);
+
+    }
 
     const onSent=async(prompt)=>{
         setResultData("");
         setLoading(true)
         setShowResult(true);
-        setRecentPrompt(input)
-        setPrevPrompts(prev=>[...prev,input])
-        const response = await run(input)
+        let response;
+        if (prompt != undefined) {
+            response= await run(prompt)
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPrevPrompts(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response=await run(input);
+        }
+        
         let responseArray= response.split("**")
         let newResponse="";
         for (let i = 0; i < responseArray.length; i++) {
@@ -59,6 +72,7 @@ const ContextProvider=(props)=>{
         resultData,
         input,
         setInput,
+        newChat
         
     }
     return(
