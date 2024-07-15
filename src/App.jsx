6ce130c1a753051bd418288be3916164dd21f702
Deepak kumar <deep98.r@gmail.com>
2024-07-15 12:57:32 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Main from './Components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    {/* <div>Hello</div> <br/> */}
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
