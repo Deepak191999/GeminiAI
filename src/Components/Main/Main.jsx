a331ed398082c9c5685d472e68e6274298aee072
Deepak kumar <deep98.r@gmail.com>
2024-07-15 18:07:08 +0530

    changeimg

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -16,7 +16,7 @@ const Main = () => {
   return (
     <div className="main">
       <div className="nav">
-        <p>Gemini aya</p>
+        <p>My Gemini</p>
         <img src={assets.user_icon} alt="user_icon" />
       </div>
       <div className="main-container">
